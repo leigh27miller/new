ca94e83565caeeb93b355868770d4ee64f61b767
Leigh Miller <leighmiller@Leighs-MacBook-Pro.local>
2018-07-03 13:38:51 -0500

    Calculator

---
+++ Calculator/src/Main.java
@@ -0,0 +1,23 @@
+import com.revature.math.Calculator;
+
+public class Main{
+    public static void main(String[] args){
+        int a, b;
+        a = Integer.parseInt(args[0]);
+        b = Integer.parseInt(args[2]);
+
+        Calculator c = new Calculator(a, b, args[1].charAt(0)); 
+        //new keywords looks through the class
+        c.calculate();
+        /*
+
+        switch(args[1].toChar()) {
+            case '+':System.out.println(a + b); break;
+            case '-': System.out.println(a - b); break;
+            case 'X': System.out.println(a * b); break;
+            case '/': System.out.println(a / b); break;
+            default: System.out.println("Error");
+    }
+    */
+
+}}
---
+++ Calculator/src/com/revature/math/Calculator.java
@@ -0,0 +1,31 @@
+package com.revature.math;
+
+public class Calculator {
+    private int a;
+    private int b;
+    private char operation;
+
+    public int sum(){
+        return a + b;
+    }
+
+    public int diff(){
+        return a - b;
+    }
+
+    public Calculator() { super(); } /*default constructor and super calls 
+    parent of parent class*/
+
+    public Calculator(int a, int b, char operation) {
+        this.a = a;
+        this.b = b;
+        this.operation = operation;
+    }
+    public void calculate (){
+        switch(operation) {
+            case '+': System.out.println(sum()); break;
+            case '-': System.out.println(diff()); break;
+
+        }
+    }
+}
